34c89b29029dc8e8be351e116bfef891a28fee22
plutownium <rolandmackintosh@gmail.com>
2020-02-10 00:12:40 -0800

    added ejs, wrote templates, .render()ed templates

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", "views"); // let express know where to find our views
+
 const blogRoutes = require("./routes/blog");
 const homeRoutes = require("./routes/home");
 
@@ -14,13 +17,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // redirect to an index of blogs
-app.use("/blog", blogRoutes);
+app.use("/blog", blogRoutes.router);
 app.use(homeRoutes);
 
 // set a 404 error page
 app.use((req, res, next) => {
 	// res.status(404).send("<h1>Page Not Found</h1>");
-	res.sendFile(path.join(__dirname, "views", "404.html"));
+	// to send a simple html file...
+	// res.sendFile(path.join(__dirname, "views", "404.html"));
+	res.render("404", {
+		pageTitle: "404 - Page Not Found",
+		h1: null,
+		posts: null
+	});
 });
 
 app.listen(3000);
--- routes/blog.js
+++ routes/blog.js
@@ -4,14 +4,34 @@ const router = express.Router();
 const path = require("path");
 const rootDir = require("../util/path");
 
-router.use("/post", (req, res, next) => {
-	res.send("<h3>You are at the Posts page.</h3>");
+const posts = [];
+
+router.get("/", (req, res, next) => {
+	// res.send("<h3>You are at the Posts page.</h3>");
+	// res.sendFile(path.join(rootDir, "views", "blog.html"));
+	res.render("blog", {
+		pageTitle: "Blog",
+		h1: "The Blog Page",
+		posts: posts
+	});
 });
 
-router.use("/", (req, res, next) => {
+router.post("/add-post", (req, res, next) => {
+	posts.push({
+		title: req.body.title,
+		content: req.body.content
+	});
 	// res.send("<h3>You are at the Index.</h3>");
 	// res.sendFile(path.join(__dirname, "../", "views", "blog.html"));
-	res.sendFile(path.join(rootDir, "views", "blog.html"));
+	// to send a simple html file...
+	// res.sendFile(path.join(rootDir, "views", "blog.html"));
+	res.render("blog", {
+		pageTitle: "Blog",
+		h1: "The Blog Page",
+		posts: posts
+	});
+	console.log(posts);
 });
 
-module.exports = router;
+exports.router = router;
+exports.posts = posts;
